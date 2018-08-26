4b9ad4a519892045801459f483e1a07905a5be9d
vnkrv <alexander.a.vinokurov@gmail.com>
2018-08-26 23:17:32 +0100

    formatting

--- salty_tickets/tokens.py
+++ salty_tickets/tokens.py
@@ -8,7 +8,7 @@ from hashids import Hashids
 
 
 def email_serialize(email):
-    serializer =  URLSafeSerializer(SECRET_KEY, salt=SALT_EMAIL)
+    serializer = URLSafeSerializer(SECRET_KEY, salt=SALT_EMAIL)
     token = serializer.dumps(email)
     return token
 
@@ -20,7 +20,7 @@ def email_deserialize(email_token):
 
 
 def order_product_serialize(order_product):
-    serializer =  Hashids(min_length=5, salt=SALT_ORDER_PRODUCT)
+    serializer = Hashids(min_length=5, salt=SALT_ORDER_PRODUCT)
     token = serializer.encode(order_product.id)
     return token
 
@@ -30,7 +30,7 @@ def order_product_deserialize(order_product_token):
     if len(order_product_token)>10:
         return order_product_deserialize_old(order_product_token)
 
-    serializer =  Hashids(min_length=5, salt=SALT_ORDER_PRODUCT)
+    serializer = Hashids(min_length=5, salt=SALT_ORDER_PRODUCT)
     decode_res = serializer.decode(order_product_token)
     if decode_res:
         order_product_id = decode_res[0]
@@ -47,32 +47,29 @@ def order_product_token_expired(order_product_token, expire_period_seconds):
 
 
 def order_product_deserialize_old(order_product_token):
-    serializer =  URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER_PRODUCT)
+    serializer = URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER_PRODUCT)
     order_product_id = serializer.loads(order_product_token)
     order_product = OrderProduct.query.filter_by(id=order_product_id).one()
     return order_product
 
 
 def order_serialize(user_order):
-    serializer =  URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER)
+    serializer = URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER)
     token = serializer.dumps(user_order.id)
     return token
 
 
 def order_deserialize(order_token):
-    serializer =  URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER)
+    serializer = URLSafeSerializer(SECRET_KEY, salt=SALT_ORDER)
     order_id = serializer.loads(order_token)
     user_order = Order.query.filter_by(id=order_id).one()
     return user_order
-
 
 
 class Token:
     prefix = 'tok'
     min_length = 5
     salt = 'default'
-    def __init__(self):
-        pass
 
     @property
     def _serializer(self):
@@ -96,7 +93,7 @@ class Token:
         else:
             raise BadSignature('Invalid token')
 
-    def _retrieve_object(self, id):
+    def _retrieve_object(self, object_id):
         raise NotImplementedError()
 
     def code_to_str(self, code):
@@ -143,35 +140,37 @@ class HashidsMixin:
 class GroupToken(HashidsMixin, Token):
     prefix = 'grp'
     salt = SALT_GROUP_TOKEN
-    def _retrieve_object(self, id):
-        return RegistrationGroup.query.filter_by(id=id).one()
+
+    def _retrieve_object(self, object_id):
+        return RegistrationGroup.query.filter_by(id=object_id).one()
 
 
 class PartnerToken(HashidsMixin, Token):
     prefix = 'ptn'
     salt = SALT_PARTNER_TOKEN
-    def _retrieve_object(self, id):
-        return Registration.query.filter_by(id=id).one()
+
+    def _retrieve_object(self, object_id):
+        return Registration.query.filter_by(id=object_id).one()
 
 
 class RegistrationToken(ItsdangerousMixin, Token):
     prefix = 'reg'
     salt = SALT_REGISTRATION_TOKEN
-    def _retrieve_object(self, id):
-        return Registration.query.filter_by(id=id).one()
+
+    def _retrieve_object(self, object_id):
+        return Registration.query.filter_by(id=object_id).one()
 
 
 class MtsTicketToken(HashidsMixin, Token):
     prefix = ''
     salt = SALT_REGISTRATION_TOKEN
     min_length = 8
-    def _retrieve_object(self, id):
-        return Registration.query.filter_by(id=id).one()
+
+    def _retrieve_object(self, object_id):
+        return Registration.query.filter_by(id=object_id).one()
 
     def code_to_str(self, code):
         return str(code)
 
     def code_from_str(self, token_str):
         return token_str
-
-
