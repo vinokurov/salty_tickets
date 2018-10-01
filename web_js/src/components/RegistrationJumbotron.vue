e5bfd81a6a863a9b771bc4babb549e0b01818bd2
vnkrv <alexander.a.vinokurov@gmail.com>
2018-10-01 08:49:25 +0100

    forgot ui file

---
+++ web_js/src/components/RegistrationJumbotron.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container-fluid">
+    <div class="container">
+      <!--Jumbotron-->
+      <div class="jumbotron text-center">
+
+        <!--Title-->
+        <h1 class="card-title h2-responsive mt-2"><strong>Salty Breezle ☆ Shag Weekend</strong></h1>
+        <!--Subtitle-->
+        <p class="blue-text mb-4 font-bold">17-18 November, 2018</p>
+
+        <!--Text-->
+        <div class="d-flex justify-content-center text-left">
+            <p class="card-text mb-3" style="max-width: 50rem;">
+              Collegiate Shag Bristol and Salty Jitterbugs (London) are incredibly excited and proud to invite you to a unique international Collegiate Shag weekend in Bristol!
+              Seize this opportunity to be taught by the stylish Kasia & Michal from Warsaw and the exuberant Annabelle & Nial from Melbourne - it’s a Shag collaboration across the Globe!
+              Learn some of their special moves, strengthen your technique, exchange ideas.
+            </p>
+        </div>
+
+        <hr class="my-4">
+        <p class="blue-text mb-4 font-bold">Workshops & Prices</p>
+        <div class="d-flex justify-content-center text-left">
+            <p class="card-text mb-3" style="max-width: 50rem;">
+                There will be 4 workshops for each level during 2 days. Each workshop is 2 hours long. You can choose to sign up for any 2, 3 or 4 workshops.
+                You can sign up on your own or as a couple.
+              </p>
+            </div>
+
+        <div class="d-flex justify-content-center text-center">
+            <p class="card-text mb-3" style="max-width: 50rem;">
+                  2 workshops (4h) - £55</br>
+                  3 workshops (6h) - £75</br>
+                  4 workshops (8h) - £90</br>
+            </p>
+        </div>
+
+        <hr class="my-4">
+        <p class="blue-text mb-4 font-bold">Waiting Lists</p>
+        <div class="d-flex justify-content-center text-left">
+            <p class="card-text mb-3" style="max-width: 50rem;">
+                In order to keep a good ratio between leads and followers in the classes we might need to apply waiting list.
+                In the registration form below options that are wait listed are indicated with orange colour.
+                <br/><br/>
+                We did our best to make waiting lists as efficient as possible.
+                We are balancing registrations automatically and accept dancers off the waiting lists as soon as possible.
+
+                <br/><br/>
+                If you are getting on the waiting list, you can choose
+                <ul>
+                  <li>Pay the whole amount in advance. You can choose to cancel your waiting listed registration and get refunded any moment.
+                  </li>
+                  <li>Leave your card details and let us charge you as soon as the place gets available. You can cancel any time while you are on the waiting list.
+                  </li>
+                </ul>
+                Please note, that we don't keep any of your card details in our system and we don't have access to your sensitive information.
+                For all payment transactions we are using Stripe API.
+            </p>
+        </div>
+
+        <hr class="my-4">
+        <p class="blue-text mb-4 font-bold">Signing up with a partner</p>
+        <div class="d-flex justify-content-center text-left">
+            <p class="card-text mb-3" style="max-width: 50rem;">
+                If you are signing up with a partner, you are guaranteed to avoid waiting lists.
+                There are 2 ways to do that.
+                <ul>
+                  <li>Book for both youself and your partner (use "Couple" button) and pay for both in one transaction.</li>
+                  <li>Sign up separately. The second one will need to specify partner token of the first one. <br/>[THIS OPTION IS NOT AVAILABLE YET]</li>
+                </ul>
+            </p>
+
+        </div>
+
+
+        <hr class="my-4">
+        <a href="https://www.facebook.com/events/306778536746949/" type="button" class="btn btn-blue waves-effect">More Info <span class="fa fa-facebook ml-1"></span></a>
+
+      </div>
+      <!--Jumbotron-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
