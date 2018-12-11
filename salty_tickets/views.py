be9400886676c13a4334aa7714a203d8a8280a4d
vnkrv <alexander.a.vinokurov@gmail.com>
2018-12-11 18:10:31 +0000

    added views

--- salty_tickets/views.py
+++ salty_tickets/views.py
@@ -7,7 +7,8 @@ from salty_tickets.api.admin import do_get_event_stats
 from salty_tickets.config import MONGO
 from salty_tickets.dao import TicketsDAO
 from salty_tickets.api.registration_process import do_price, do_checkout, do_pay, do_get_payment_status, \
-    do_check_partner_token, EventInfo, balance_event_waiting_lists
+    do_check_partner_token, EventInfo, balance_event_waiting_lists, do_validate_discount_code_token, \
+    do_validate_registration_group_token, do_create_registration_group
 from salty_tickets.api.user_order import do_get_user_order_info
 from salty_tickets.utils.utils import jsonify_dataclass
 
@@ -83,6 +84,25 @@ def user_order_index(pmt_token):
 def user_order_info(pmt_token):
     dao = TicketsDAO(MONGO)
     return jsonify_dataclass(do_get_user_order_info(dao, pmt_token))
+
+
+@app.route('/check_discount_token/<string:event_key>', methods=['POST'])
+def check_discount_token(event_key):
+    dao = TicketsDAO(MONGO)
+    return jsonify_dataclass(do_validate_discount_code_token(dao, event_key))
+
+
+@app.route('/check_registration_group_token/<string:event_key>', methods=['POST'])
+def check_registration_group_token(event_key):
+    dao = TicketsDAO(MONGO)
+    return jsonify_dataclass(do_validate_registration_group_token(dao, event_key))
+
+
+@app.route('/create_registration_group/<string:event_key>', methods=['POST'])
+def create_registration_group(event_key):
+    dao = TicketsDAO(MONGO)
+    return jsonify_dataclass(do_create_registration_group(dao, event_key))
+
 
 #####################################################################
 #########                A D M I N                   ################
