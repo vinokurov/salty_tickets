8c0eef78ccb2b63d542a6d2cce711c816aa7fe7d
vnkrv <alexander.a.vinokurov@gmail.com>
2019-02-01 00:18:13 +0000

    adding Jose to Shag Clinic

---
+++ mail_scripts/maintenance_20190201_add_jose_shag_clinic.py
@@ -0,0 +1,33 @@
+from salty_tickets.api.registration_process import set_payment_totals
+from salty_tickets.config import MONGO
+from salty_tickets.constants import LEADER, SUCCESSFUL
+from salty_tickets.dao import TicketsDAO
+from salty_tickets.models.registrations import Payment, Registration
+
+dao = TicketsDAO(host=MONGO)
+event = dao.get_event_by_key('mind_the_shag_2019')
+person = dao.get_person_by_id("5c110cbbb01bad00409a4d42")
+
+registrations = [Registration(
+        person=person,
+        registered_by=person,
+        ticket_key='shag_clinic',
+        dance_role=LEADER,
+        price=0,
+        paid_price=0,
+        is_paid=True,
+        active=True
+    )]
+
+payment = Payment(
+    person=person,
+    registrations=registrations,
+    status=SUCCESSFUL,
+    info_items=[(event.tickets[r.ticket_key].item_info(r), r.price) for r in registrations],
+    pay_all_now=True,
+)
+set_payment_totals(payment)
+
+import pdb;pdb.set_trace()
+dao.add_payment(payment, event, register=True)
+
